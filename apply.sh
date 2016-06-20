c49f2d5848f1e28d2a58561bc35f25a6ee9bfadb
Hanjoung Lee <hanjoung.lee@samsung.com>
2016-06-20 14:51:53 +0900

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+# Copy files to home directory
+
 cp -rTv homedir ~
+
+
+# Changes in bashrc
+
+BASHRC_PATH="${HOME}/.bashrc"
+BASHRC_MORE="source ~/.bashrc_more"
+
+grep "'${BASHRC_MORE}'" ${BASHRC_PATH}
+if [ $? -ne 0 ]; then
+	echo "# additional settings for bashrc" >> ${BASHRC_PATH}
+	echo "${BASHRC_MORE}" >> ${BASHRC_PATH}
+fi
