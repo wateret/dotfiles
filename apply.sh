e8bf19a400972a3a23d2d3bd725926bd2d76f6fd
Hanjoung Lee <hanjoung.lee@samsung.com>
2016-09-22 15:53:29 +0900

    Update apply.sh for ZSH

--- apply.sh
+++ apply.sh
@@ -9,12 +9,13 @@ elif [[ $UNAME == 'Linux' ]]; then # assuming Ubuntu
 	INSTALL='sudo apt-get install '
 fi
 
-# Copy files to home directory
+
+### Copy files to home directory
 
 rsync -av homedir/ ~/
 
 
-# Changes in bashrc
+### Changes in bashrc
 
 BASHRC_PATH="${HOME}/.bashrc"
 BASHRC_MORE="source ~/.bashrc_more"
@@ -26,11 +27,21 @@ if [ $? -ne 0 ]; then
 fi
 
 
-# ZSH
+### Install fundamental programs
+
+$INSTALL vim git
+
+
+### ZSH
 
 # install zsh
-$INSTALL zsh zsh-completions
+$INSTALL zsh
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install zsh plugins
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
