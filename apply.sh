f5b21f5b88e9af39a7d1cf87a80c86dee7485b65
Hanjoung Lee <waterets@gmail.com>
2020-01-05 00:18:46 +0900

    Update
    Signed-off-by: Hanjoung Lee <waterets@gmail.com>

--- apply.sh
+++ apply.sh
@@ -59,4 +59,5 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/bhilburn/powerlevel9k.git $ZSH_CUSTOM/themes/powerlevel9k
 
