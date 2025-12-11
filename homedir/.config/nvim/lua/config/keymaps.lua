8792ced403032fef21fa456d1cce369f33f5f24b
Hanjoung Lee <waterets@gmail.com>
2025-12-11 23:19:59 +0900

    Update

--- homedir/.config/nvim/lua/config/keymaps.lua
+++ homedir/.config/nvim/lua/config/keymaps.lua
@@ -6,5 +6,5 @@ vim.keymap.set("n", "<leader>oe", "<cmd>Neotree focus<cr>", { desc = "Focus Neo-
 
 -- osc52 Keymaps
 vim.keymap.set("n", "<leader>y", require("osc52").copy_operator, { expr = true })
-vim.keymap.set("n", "<leader>yy", "<leader>c_", { remap = true })
+vim.keymap.set("n", "<leader>yy", "<leader>y_", { remap = true })
 vim.keymap.set("v", "<leader>y", require("osc52").copy_visual)
