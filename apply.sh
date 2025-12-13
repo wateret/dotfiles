aaaf4ad65ada6daf3808c75bd277e0b6ad828e1f
Hanjoung Lee <waterets@gmail.com>
2025-12-14 01:07:49 +0900

    Update

--- apply.sh
+++ apply.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
+UNAME=$(uname)
 
-UNAME=`uname`
+if [[ $UNAME == 'Darwin' ]]; then # macOS
+  defaults write -g ApplePressAndHoldEnabled -bool false
+  defaults write -g com.apple.mouse.scaling -1
 
-if [[ $UNAME == 'Darwin' ]]; then  # macOS
-    defaults write -g ApplePressAndHoldEnabled -bool false
-    defaults write -g com.apple.mouse.scaling -1
-
-    # install brew
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    INSTALL='brew install '
+  # install brew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  INSTALL='brew install '
 elif [[ $UNAME == 'Linux' ]]; then # Linux (assuming Ubuntu)
-    INSTALL='sudo apt-get install -y'
+  INSTALL='sudo apt-get install -y'
 fi
-
 
 ### Copy files to home directory
 
 rsync -av homedir/ ~/
-
 
 ### Changes in bashrc
 
@@ -27,12 +24,11 @@ BASHRC_MORE="source ~/.bashrc_more"
 
 grep "'${BASHRC_MORE}'" ${BASHRC_PATH}
 if [ $? -ne 0 ]; then
-    echo
-    echo
-    echo "# additional settings for bashrc" >> ${BASHRC_PATH}
-    echo "${BASHRC_MORE}" >> ${BASHRC_PATH}
+  echo
+  echo
+  echo "# additional settings for bashrc" >>${BASHRC_PATH}
+  echo "${BASHRC_MORE}" >>${BASHRC_PATH}
 fi
-
 
 ### Install fundamental programs
 
@@ -40,6 +36,7 @@ $INSTALL vim
 $INSTALL neovim
 $INSTALL git
 $INSTALL ripgrep
+$INSTALL stow
 
 #### zoxide
 
@@ -68,7 +65,5 @@ git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/z
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
-
-### Copy files to home directory again (for .zshrc)
-
-rsync -av homedir/ ~/
+### Create symlinks for all dotfiles
+stow
