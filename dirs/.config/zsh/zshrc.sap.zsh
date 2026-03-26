75299e847f843bc0d216371d34e1d52239306517
Hanjoung Lee <hanjoung.lee@sap.com>
2026-03-26 21:41:35 +0900

    SAP-specific

---
+++ dirs/.config/zsh/zshrc.sap.zsh
@@ -0,0 +1,14 @@
+# SAP specific settings
+
+for i in {0..99}; do
+    alias db$i="sudo su - h$(printf '%02d' $i)adm -c"
+    alias dbc$i="hdbsql -i ${i} -u system -p manager -fj \"alter system clear traces ('*');\" && sudo su - h$(printf '%02d' $i)adm -c"
+    alias hs$i="hdbsql -i ${i} -u system -p manager -fj"
+    alias hsc$i="hdbsql -i ${i} -u system -p manager -fj \"alter system clear traces ('*');\" && hdbsql -i ${i} -u system -p manager -fj"
+done
+
+export PATH="/data/i565578/.bin:$PATH"
+export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
+
+source /data/i565578/.HappyMake/etc/hminit.sh
+alias clc="claude --model \"opus[1m]\""
